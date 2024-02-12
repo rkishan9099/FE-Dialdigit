807564dee8ad247a2de56e1e1476dd08511f1f5d
Kishan Ramani <rkishan9099@gmail.com>
2024-02-12 21:37:05 +0530

    -->Fix login  issue

--- app/(main)/dashboards/page.tsx
+++ app/(main)/dashboards/page.tsx
@@ -10,31 +10,15 @@ import React from "react";
 import { useDispatch } from "react-redux";
 
 const DashboardPage = () => {
-  const { user } = useAuth();
-  const media = useMediaQuery("sm");
-  const dispatch = useDispatch<AppDispatch>();
   const { sessionCount } = useSipSessionManager();
-  // const { u
 
-  const callApi = async () => {
-    const res = await getUser({ name: "kishan" });
-    console.log("res", res);
-    //  console.log(await auth())
-  };
   return (
     <>
-      <div>Welcome {user?.name}</div>
-      <Button onClick={callApi}>Call Api</Button>
       <Grid container spacing={3}>
         <Grid item xs={12} md={4} sm={6}>
           {sessionCount() > 0 ? <OngoingCall /> : <CallDialpad />}
         </Grid>
       </Grid>
-      {/* <Stack>
-       
-      
-       
-      </Stack> */}
       <Grid container>
         <Grid item xs={4}></Grid>
         <Grid item xs={8}></Grid>
